f0b46de87593c9f6a6fa756a0140995219218561
uwantwater <aradhyadhungelr@gmail.com>
2023-05-03 21:31:05 +0545

    db

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -4,4 +4,6 @@ import axios from 'axios'
 
 import App from './App';
 
+const promise = axios.get('http://localhost:3001/persons')
+console.log(promise)
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
